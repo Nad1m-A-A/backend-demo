4bac36129bb26039e7c49c33b66a16022bfa04a7
nadim <nadim.alaa@hotmail.com>
2024-03-01 05:29:06 +0400

    added delete method. fixed error issue in chemicals controllers

--- controllers/chemicalsCont.js
+++ controllers/chemicalsCont.js
@@ -4,12 +4,12 @@ const getChemicals = async (req, res) => {
     const chemicals = await chemicalsDoc.find({});
     return res.status(200).json(chemicals);
   } catch (error) {
-    return res.status(500).json({ error });
+    return res.status(500).json({ error: error.message });
   }
 };
 
 const addChemical = async (req, res) => {
-    const chemicalName = req.body.name;
+  const chemicalName = req.body.name;
   try {
     await chemicalsDoc.create({
       name: chemicalName,
@@ -19,9 +19,12 @@ const addChemical = async (req, res) => {
       message: `${chemicalName} was added successfully!`,
     });
   } catch (error) {
-    console.log(error.code);
-    if(error.code === 11000) return res.status(500).json({success: false, message:`Duplicate key. ${chemicalName} already exists`});
-    return res.status(500).json(error);
+    if (error.code === 11000)
+      return res.status(500).json({
+        success: false,
+        message: `Duplicate key. ${chemicalName} already exists`,
+      });
+    return res.status(500).json({ error: error.message });
   }
 };
 
@@ -30,14 +33,29 @@ const updateChemical = async (req, res) => {
     const chemicalToUpdate = await chemicalsDoc.findByIdAndUpdate(
       req.params.id,
       { count: req.body.count },
-      { new: true}
+      { new: true }
     );
     return res.status(200).json({
       success: true,
       message: `${chemicalToUpdate.name} count was updated!`,
     });
   } catch (error) {
-    return res.status(500).json(error);
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+const deleteChemical = async (req, res) => {
+  try {
+    const chemicalToDelete = await chemicalsDoc.findByIdAndDelete(
+      req.params.id
+    );
+    if (!chemicalToDelete) throw new Error("chemical was not found");
+    return res.status(200).json({
+      success: true,
+      message: `${chemicalToDelete.name} was deleted!`,
+    });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
   }
 };
 
@@ -45,4 +63,5 @@ module.exports = {
   getChemicals,
   addChemical,
   updateChemical,
+  deleteChemical,
 };
--- routes/chemicalsRoute.js
+++ routes/chemicalsRoute.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const {getChemicals, addChemical, updateChemical} = require('../controllers/chemicalsCont');
+const {getChemicals, addChemical, updateChemical, deleteChemical} = require('../controllers/chemicalsCont');
 router.route("/").get(getChemicals).post(addChemical);
-router.route('/:id').patch(updateChemical)
+router.route('/:id').patch(updateChemical).delete(deleteChemical);
 
 module.exports = router;
