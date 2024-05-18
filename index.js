a9b8609d6acca66852d8cbf3994777d3d802a281
nadim <nadim.alaa@hotmail.com>
2024-05-18 06:21:39 +0400

    created UI

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 
@@ -11,7 +12,7 @@ const alloyRoute = require("./routes/alloyRoute");
 const collectionsRoute = require("./routes/collectionsRoute");
 const errorHandler = require("./utils/errorHandler");
 
-app.use(express.json())
+app.use(express.json());
 app.use(cors());
 app.use("/chemicals", chemicalsRoute);
 app.use("/shapes", shapesRoute);
@@ -23,7 +24,57 @@ app.use(errorHandler);
 connectDB();
 
 app.get("/", (req, res) => {
-  return res.status(200).send("Welcome To Al-K6 Management.");
+  res.setHeader("Content-Type", "text/html");
+  return res.status(200).send(`
+    <html>
+      <head>
+        <style>
+          body {
+            font-family: Arial, sans-serif;
+            text-align: center;
+            margin: 0;
+            padding: 0;
+            background-color: #f4f4f9;
+          }
+          h1 {
+            color: #333;
+            margin-top: 50px;
+          }
+          a {
+            margin: 20px 0;
+            color: #fff;
+            text-decoration: none;
+            font-size: 18px;
+            padding: 1rem 2rem;
+            background: #333;
+            border-radius: 3px;
+          }
+          a:hover {
+            text-decoration: underline;
+          }
+          .container {
+            padding: 20px;
+          }
+          .links-container {
+            display: flex;
+            justify-content: center;
+            gap: 1rem;
+          }
+        </style>
+      </head>
+      <body>
+        <div class="container">
+          <h1>Welcome To Al-K6 Management.</h1>
+          <div class="links-container">
+          <a href="${process.env.APP_BASE_URL}/alloy">Alloy</a>
+          <a href="${process.env.APP_BASE_URL}/shapes">Shapes</a>
+          <a href="${process.env.APP_BASE_URL}/orders">Orders</a>
+          <a href="${process.env.APP_BASE_URL}/chemicals">Chemicals</a>
+          </div>
+        </div>
+      </body>
+    </html>
+  `);
 });
 
 const port = 5000;
